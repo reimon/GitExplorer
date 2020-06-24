2dd98fc54287087f17b7637ed411f5745533c84f
Reimon Ferreira <reimonf@gmail.com>
2020-06-24 20:23:38 -0300

    :rocket: finish

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+/* eslint-disable camelcase */
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
@@ -10,22 +12,51 @@ import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
+}
 
+interface Repository {
+  full_name: string;
   description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
 }
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>();
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
 
+  const { params } = useRouteMatch<RepositoryParams>();
   useEffect(() => {
     api.get(`repos/${params.repository}`).then((response) => {
-      console.log(response.data);
+      setRepository(response.data);
     });
-  }, [params.repository]);
 
-  useEffect(() => {
     api.get(`repos/${params.repository}/issues`).then((response) => {
-      console.log(response.data);
+      setIssues(response.data);
     });
+
+    // async function loadData(): Promise<void> {
+    //   const [respository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+    //   console.log(respository);
+    //   console.log(issues);
+    // }
+    // loadData();
   }, [params.repository]);
   return (
     <>
@@ -36,40 +67,44 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars0.githubusercontent.com/u/6543699?v=4"
-            alt="Logo"
-          />
-          <div>
-            <strong>Rocketseat/unform</strong>
-            <p>descrição do repositorio</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Starts</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>18</strong>
-            <span>Issues abertos</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Starts</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertos</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
       <Issues>
-        <Link to="asdfg">
-          <div>
-            <strong>adsf</strong>
-            <p>dfghad</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
+        {issues.map((issue) => (
+          <a key={issue.id} target="_blank" href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
